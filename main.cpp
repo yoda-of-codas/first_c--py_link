44477bff5988c6730a359bf9221beef729ce06a5
yoda-of-codas <yoda_of_codas@protonmail.com>
2020-09-03 13:53:30 +0100

    The Full Matter

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <Python.h>
+#include <pyhelper.hpp>
+
+int main()
+{
+	CPyInstance hInstance;
+
+	CPyObject pName = PyUnicode_FromString("nice");
+	CPyObject pModule = PyImport_Import(pName);
+
+	if(pModule)
+	{
+		CPyObject pFunc = PyObject_GetAttrString(pModule, "getInteger");
+		if(pFunc && PyCallable_Check(pFunc))
+		{
+			CPyObject pValue = PyObject_CallObject(pFunc, NULL);
+
+			printf("C: getInteger() = %ld\n", PyLong_AsLong(pValue));
+		}
+		else
+		{
+			printf("ERROR: function getInteger()\n");
+		}
+
+	}
+	else
+	{
+		printf("ERROR: ff Module not imported\n");
+	}
+
+	return 0;
+}
---
+++ sample.py
@@ -0,0 +1,6 @@
+    # Returns the sum of two numbers.
+
+    def add(a, b):
+
+        return a+b
+
